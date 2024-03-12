844d72b859b84045aed4635a2e5e848b190f62db
SupunOshadha <supunoshadha23@gmail.com>
2024-03-12 13:38:15 +0530

    udating(style-12)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
           </tbody>
         </table>
       )}
-      <footer className="w-full">
+      <footer className="w-full absolute top-20">
         <div className="h-20 bg-sky-950 flex justify-center items-center">
               <ul className="text-white text-center">
                 <li className="text-center">created by @supun-NexNova</li>
