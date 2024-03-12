bf046351409e956c64944ad8be55858f1cca8761
SupunOshadha <supunoshadha23@gmail.com>
2024-03-12 10:02:58 +0530

    updating(3)

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 export const Port = process.env.PORT || 5000;
 
-export const mongoDBURL = "mongodb+srv://root:Soktcroxsr123@book-store-mern.agnzjzi.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN";
+// export const mongoDBURL = "mongodb+srv://root:Soktcroxsr123@book-store-mern.agnzjzi.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN";
 
+export const mongoDBURL =  process.env.MONGODB_URL || "mongodb+srv://root:Soktcroxsr123@book-store-mern.agnzjzi.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN";
