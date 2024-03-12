dd61a47236cdb9e24247eb0d3d21cc2cd9e3f706
SupunOshadha <supunoshadha23@gmail.com>
2024-03-12 11:56:45 +0530

    Adding-bg-img

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import ShopImage from "../img/shop-img.jpg";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -27,7 +28,7 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="p-4">
+    <div className="p-4 bg-no-repeat bg-cover" style={{backgroundImage: `url(${ShopImage})`}}>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
